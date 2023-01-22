2f9fb4cd55674bd5e148a9440e14919786d6cdb2
abdul <abdulrehman133782@gmail.com>
2023-01-23 10:25:47 +1100

    added sass and fixed bug that did not allow sass to run correctly

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
@@ -26,7 +29,7 @@ const App = () => {
     <div className="categories-container">
       {categories.map(({ title }) => (
         <div className="category-container">
-          {/*<img />*/}
+          <div className="background-image" />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
